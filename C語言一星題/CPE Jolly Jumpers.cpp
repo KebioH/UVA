a06febb9bcbb47c623e83309b55e1c8289795d2a
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-08 01:49:58 +0800

    Add files via upload

---
+++ C語言一星題/CPE Jolly Jumpers.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        int* arr = (int*)malloc(n * sizeof(n));
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                if (abs(arr[j] - arr[j - 1]) == i) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        if (count == n - 1) {
+            printf("Jolly\n");
+        }
+        else {
+            printf("Not jolly\n");
+        }
+    }
+}
+
