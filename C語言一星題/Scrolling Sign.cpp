2348c3070a0b3e645732bc6462473dca4728b2c8
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-11 22:20:53 +0800

    Create Scrolling Sign.cpp

---
+++ C語言一星題/Scrolling Sign.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    while (n--) {
+        int k, w;
+        scanf("%d%d", &k, &w);
+
+        char arr[100][100];
+        for (int i = 0; i < w; i++) {
+            scanf("%s", arr[i]);
+        }
+
+        int sum = k; //第一個單字完整顯示
+        for (int i = 0; i < w - 1; i++) {
+            int max = 0; //還是習慣給初始值 不要吝嗇
+            for (int j = 1; j <= k; j++) {
+                if (strncmp(arr[i] + k - j, arr[i + 1], j) == 0) { //學到了指標運算
+                    max = j;
+                }
+            }
+
+            sum += k - max;
+        }
+        printf("%d\n", sum);
+    }
+}
