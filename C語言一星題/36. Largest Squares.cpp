4c147ab0e45adadbed57473b87e72f7e3ab41418
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-16 15:25:19 +0800

    Update 36. Largest Squares.cpp

--- C語言一星題/36. Largest Squares.cpp
+++ C語言一星題/36. Largest Squares.cpp
@@ -1,49 +1,43 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-        int m, n, q;
-        scanf("%d%d%d", &m, &n, &q);
-        char matrix[100][100];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                scanf(" %c", &matrix[i][j]); //空白好像很好用
-            }
-        }
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int m, n, q;
+		scanf("%d%d%d", &m, &n, &q);
 
-        printf("%d %d %d\n", m, n, q);
-        while (q--) {
-            int r, c;
-            scanf("%d%d", &r, &c);
-            
-            int d = 1, size = 1;
-            while (r - d >= 0 && r + d < m && c - d >= 0 && c + d < n) { //這招蠻好用的 學起來
-                bool valid = true;
-                for (int i = c - d; i <= c + d; i++) {
-                    if (matrix[r - d][i] != matrix[r][c] || matrix[r + d][i] != matrix[r][c]) { //檢查最外圍
-                        valid = false;
-                        break;
-                    }
-                }
-                for (int i = r - d; i <= r + d; i++) {
-                    if (matrix[i][c - d] != matrix[r][c] || matrix[i][c + d] != matrix[r][c]) {
-                        valid = false;
-                        break;
-                    }
-                }
+		char arr[110][110];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				scanf(" %c", &arr[i][j]); //scanf會讀取\n 所以要在%c前面加空白鍵
+			}
+		}
 
-                if (!valid) break;
+		printf("%d %d %d\n", m, n, q);
+		while (q--) {
+			int r, c;
+			scanf("%d%d", &r, &c);
 
-                size += 2;
-                d++;
-            }
-            
-            printf("%d\n", size);
-        }
-    }
+			int d = 1, l = 1;
+			while (r - d >= 0 && r + d < m && c - d >= 0 && c + d < n) { //記得檢查條件
+				for (int i = r - d; i <= r + d; i++) { //跟minesweeper一樣的判斷方法
+					for (int j = c - d; j <= c + d; j++) {
+						if (arr[i][j] != arr[r][c]) {
+							goto output;
+						}
+					}
+				}
+			
+				d++;
+				l += 2;
+			}
+
+		output:;
+			printf("%d\n", l);
+		}
+	}
+
 }
