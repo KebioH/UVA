449072df759e09ae39e0c8b83b0196cc781e2162
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-16 22:33:52 +0800

    Update 39. Fourth Point.cpp

--- C語言一星題/39. Fourth Point.cpp
+++ C語言一星題/39. Fourth Point.cpp
@@ -1,32 +1,33 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    double x[4], y[4];
-    double x1, y1, x2, y2, x3, y3;
-    while (scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &x[0], &y[0], &x[1], &y[1], &x[2], &y[2], &x[3], &y[3]) != EOF) {
-        if (x[1] == x[2] && y[1] == y[2]) {
-            x1 = x[0]; y1 = y[0];
-            x2 = x[1]; y2 = y[1];
-            x3 = x[3]; y3 = y[3];
-        }
-        else if (x[0] == x[2] && y[0] == y[2]) {
-            x1 = x[1]; y1 = y[1];
-            x2 = x[0]; y2 = y[0];
-            x3 = x[3]; y3 = y[3];
-        }
-        else if (x[0] == x[3] && y[0] == y[3]) {
-            x1 = x[1]; y1 = y[1];
-            x2 = x[0]; y2 = y[0];
-            x3 = x[2]; y3 = y[2];
-        }
-        else if (x[1] == x[3] && y[1] == y[3]) {
-            x1 = x[0]; y1 = y[0];
-            x2 = x[1]; y2 = y[1];
-            x3 = x[2]; y3 = y[2];
-        }
-        printf("%.3f %.3f\n", x3 + x1 - x2, y3 + y1 - y2);
-    }
-    return 0;
-}
+    double x1, y1, x2, y2, x3, y3, x4, y4;
 
+    while (scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3, &x4, &y4) == 8) {
+        double px, py;
+
+        if (x1 == x3 && y1 == y3) {
+            // A == C，第四點 P = D + B - A
+            px = x4 + x2 - x1;
+            py = y4 + y2 - y1;
+        }
+        else if (x1 == x4 && y1 == y4) {
+            // A == D，第四點 P = C + B - A
+            px = x3 + x2 - x1;
+            py = y3 + y2 - y1;
+        }
+        else if (x2 == x3 && y2 == y3) {
+            // B == C，第四點 P = D + A - B
+            px = x4 + x1 - x2;
+            py = y4 + y1 - y2;
+        }
+        else {
+            // B == D，第四點 P = C + A - B
+            px = x3 + x1 - x2;
+            py = y3 + y1 - y2;
+        }
+
+        printf("%.3lf %.3lf\n", px, py);
+    }
+}
