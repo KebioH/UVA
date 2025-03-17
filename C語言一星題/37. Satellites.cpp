9d04c2c47089cfaff2b386614f73bdd7e6f9f27b
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-18 01:36:12 +0800

    Update 37. Satellites.cpp

--- C語言一星題/37. Satellites.cpp
+++ C語言一星題/37. Satellites.cpp
@@ -1,28 +1,28 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 
-#define PI 3.14159265358979323846
+//弦長 = 2*R*sin(theta/2)
+
 #define R 6440
+#define Pi 3.14159265358979323846
 
-int main()
-{
-    int s, a;
-    char str[4];
-    while (scanf("%d%d%s", &s, &a, str) != EOF) {
-        double angle;
-
-        if (str[0] == 'd') { //度轉弧度 
-            angle = a * PI / 180; //360度 = 2*pi弧度    1度= pi/180
+int main() {
+    double s, a;
+    char str[10];
+    while (scanf("%lf%lf%s", &s, &a, str) != EOF) {
+        
+        double r = s + R, angle;
+        if (str[0] == 'd') {
+            angle = a * Pi / 180;
         }
         else {
-            angle = a * PI / 10800; //pi/180*60
+            angle = a * Pi / (180*60);
         }
 
-        double arc = (s + R) * angle;
-        double chord = 2 * (s + R) * sin(angle / 2);
-
+        double arc = r * angle;
+        double chord = 2 * r * sin(angle / 2);
+    
         printf("%.6f %.6f\n", arc, chord);
-        
     }
 }
