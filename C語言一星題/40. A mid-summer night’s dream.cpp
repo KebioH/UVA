45f6c619ebcfbf694833ca84a89eef2e4c428d85
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-18 01:01:02 +0800

    Update 40. A mid-summer night’s dream.cpp

--- C語言一星題/40. A mid-summer night’s dream.cpp
+++ C語言一星題/40. A mid-summer night’s dream.cpp
@@ -1,39 +1,47 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-int compare(const void* x, const void* y) {
-    return *(int*)x - *(int*)y;
+//第一個數字是能得到該算式最小值的A。
+//第二個數字是 | Xi − A | 為最小值的數量。
+//第三行數字是有多少不同值可以讓總距離為最小。
+
+int compare(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
 }
 
-int main()
-{
+int main() {
     int n;
     while (scanf("%d", &n) != EOF) {
-        int* x = (int*)malloc(n * sizeof(int));
+        int* xi = (int*)malloc(n * sizeof(int));
         for (int index = 0; index < n; index++) {
-            scanf("%d", &x[index]);
+            scanf("%d", &xi[index]);
         }
-        qsort(x, n, sizeof(int), compare);
+        qsort(xi, n, sizeof(int), compare);
 
-        int a, count = 0, mid = 0;
-        if (n % 2) {
-            a = x[n / 2];
+        int a, count = 0, mid;
+        if (n % 2 == 1) {
+            a = xi[n / 2];
             for (int i = 0; i < n; i++) {
-                if (x[i] == a) count++;
+                if (xi[i] == a) {
+                    count++;
+                }
             }
             mid = 1;
-        } else {
-            int L = x[(n / 2) - 1];
-            int R = x[n / 2];
-            a = L; //因為前面已經整理過了
+        }
+        else {
+            int l = xi[n / 2 - 1];
+            int r = xi[n / 2];
+            a = l;
 
             for (int i = 0; i < n; i++) {
-                if (x[i] >= L && x[i] <= R) count++;
+                if (xi[i] >= l && xi[i] <= r) {
+                    count++;
+                }
             }
-            mid = R - L + 1;
+            mid = r - l + 1;
         }
         printf("%d %d %d\n", a, count, mid);
-        free(x);
+        free(xi);
     }
 }
