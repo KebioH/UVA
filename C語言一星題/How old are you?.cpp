7de510521d0cf492ad9b033ad258ae76e82bf851
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-18 11:44:31 +0800

    Create How old are you?.cpp

---
+++ C語言一星題/How old are you?.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int t, index = 0;
+    scanf("%d", &t);
+    while (t--) {
+        getchar();
+        index++;
+        char now[20], birth[20];
+        int nowD, nowM, nowY, birD, birM, birY;
+        scanf("%s%s", now, birth);
+        sscanf(now, "%d/%d/%d", &nowD, &nowM, &nowY);
+        sscanf(birth, "%d/%d/%d", &birD, &birM, &birY);
+        
+        if (nowY > birY) {
+            int y = nowY - birY;
+            if (nowM > birM || (nowM == birM && nowD >= birD)) {
+                if (y <= 130) {
+                    printf("Case #%d: %d\n", index, nowY - birY);
+                } else {
+                    printf("Case #%d: Check birth date\n", index);
+                }
+            } else {
+                printf("Case #%d: %d\n", index, nowY - birY - 1);
+            }
+        } else if (nowY == birY) {
+            if (nowM > birM) {
+                printf("Case #%d: 0\n", index);
+            } else if (nowM == birM){
+                if (nowD >= birD) {
+                    printf("Case #%d: 0\n", index);
+                } else {
+                    printf("Case #%d: Invalid birth date\n", index);
+                }
+            } else {
+                printf("Case #%d: Invalid birth date\n", index);
+            }
+        } else {
+            printf("Case #%d: Invalid birth date\n", index);
+        }
+    }
+}
