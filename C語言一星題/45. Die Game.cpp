ec0b6778ae7260f06d5b056d015ec7106886d8a5
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-18 23:01:48 +0800

    Update 45. Die Game.cpp

--- C語言一星題/45. Die Game.cpp
+++ C語言一星題/45. Die Game.cpp
@@ -1,50 +1,36 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
-int main()
-{
+int main() {
     int n;
     while (scanf("%d", &n) == 1 && n != 0) {
-        int top = 1, north = 2, west = 3, east = 4, south = 5, bottom = 6;
-        //只要知道top,west,north即可，其餘的用和為7來推~
-
+        int top = 1, north = 2, west = 3;
         while (n--) {
-            char pos[10];
-            scanf("%s", pos);
-            int temp; //精隨阿
+            char command[10];
+            scanf("%s", command);
 
-            if (strcmp(pos, "north") == 0) {
+            int temp;
+            if (command[0] == 'e') {
                 temp = top;
-                top = south;
-                south = bottom;
-                bottom = north;
-                north = temp; //精隨阿
-
+                top = west;
+                west = 7 - temp;
             }
-            else if (strcmp(pos, "west") == 0) {
-                temp = top;
-                top = east;
-                east = bottom;
-                bottom = west;
-                west = temp;
-            }
-            else if (strcmp(pos, "south") == 0) {
+            else if (command[0] == 's') {
                 temp = top;
                 top = north;
-                north = bottom;
-                bottom = south;
-                south = temp;
+                north = 7 - temp;
+            }
+            else if (command[0] == 'w') {
+                temp = top;
+                top = 7 - west;
+                west = temp;
             }
             else {
                 temp = top;
-                top = west;
-                west = bottom;
-                bottom = east;
-                east = temp;
+                top = 7 - north;
+                north = temp;
             }
         }
         printf("%d\n", top);
     }
 }
-
