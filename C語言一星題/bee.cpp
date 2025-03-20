70bdd4c770f262cb62a2b0d43f5c850d3759e8ea
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-20 22:14:59 +0800

    Create bee.cpp

---
+++ C語言一星題/bee.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//看懂題目很好寫 斷句要斷對
+
+int main() {
+    int n;
+    while (scanf("%d", &n) == 1 && n != -1) {
+        long long int male = 0, female = 1, temp;
+        while (n--) { // 費氏數列
+            temp = male;
+            male += female;
+            female = temp + 1; //(第一支母蜂 所以+1)
+        }
+        printf("%lld %lld\n", male, male + female);
+    }
+}
