03a7c4c6cb8503bbbe5370f092598c45e5a0fcdb
KebioH <78331057+KebioH@users.noreply.github.com>
2025-03-23 21:10:16 +0800

    Update 29. Cheapest Base.cpp

--- C語言一星題/29. Cheapest Base.cpp
+++ C語言一星題/29. Cheapest Base.cpp
@@ -1,57 +1,44 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
 
-int main()
-{
-    int c;
-    scanf("%d", &c);
-    for (int index = 1; index <= c; index++) {
-        int cost[36];
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    
+    int cost[36];
+    for (int index = 1; index <= n; index++) {
         for (int i = 0; i < 36; i++) {
             scanf("%d", &cost[i]);
         }
-        
-        int n;
-        scanf("%d", &n);
-        int* num = (int*)malloc(n * sizeof(int));
-
         printf("Case %d:\n", index);
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &num[i]);
-            int arr[100], sumArr[40], min = INT_MAX, minBase;
-            for (int base = 2; base <= 36; base++) {
-                int temp = num[i], size = 0, sum = 0;
-                if (temp == 0) arr[size++] = 0; //因為0就是0
-                while (temp != 0) { //進位制的位元就這樣取 很神奇
-                    arr[size++] = temp % base; //使用「除以𝑏取餘數」的方式，不斷除到商數為 0，收集所有餘數並逆序，就得到 𝑁 在 base b 下的 digit 序列。
-                    temp /= base;
-                }
+
+        scanf("%d", &m);
+        while (m--) {
+            int num, sum[37] = { 0 };
+            scanf("%d", &num);
+
+            int min = INT_MAX;
+            for (int i = 2; i <= 36; i++) {
+                int temp = num;
+
+                if (temp == 0) sum[i] += cost[0]; //又漏掉處理0了
                 
-                for (int j = 0; j < size; j++) {
-                    sum += cost[arr[j]]; //確實這樣寫很方便 有時候不知道要不要用兩個迴圈就這樣寫
-                    sumArr[base] = sum;
+                while (temp != 0) {
+                    sum[i] += cost[temp % i];
+                    temp /= i;
                 }
-                if (min > sum) min = sum;
+                if (min > sum[i]) min = sum[i]; //這個要放在外面
             }
 
-            printf("Cheapest base(s) for number %d: ", num[i]);
-            for (int base = 2; base <= 36; base++) {
-                if (sumArr[base] == min) {
-                    printf("%d ", base);
+            printf("Cheapest base(s) for number %d: ", num);
+            for (int i = 2; i <= 36; i++) {
+                if (sum[i] == min) {
+                    printf("%d ", i);
                 }
             }
             printf("\n");
         }
         printf("\n");
-        free(num);
     }
 }
-
-
-
-
-
-
-
